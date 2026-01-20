b36cac2db59417c548ef78cddc265a5da5ad2ad6
gjermundhp02 <gjermund.pedersen@gmail.com>
2026-01-20 22:59:05 +0100

    feat: add github runner

---
+++ hosts/github-runner/configuration.nix
@@ -0,0 +1,16 @@
+{...}:{
+  fileSystems."/mnt/truenas" = {
+    device = "192.168.1.107:/mnt/storage0/iso";
+    fsType = "nfs";
+  };
+  # optional, but ensures rpc-statsd is running for on demand mounting
+  boot.supportedFilesystems = [ "nfs" ];
+  services.github-runners = {
+    proxmox1 = {
+      url = "https://github.com/Login-Linjeforening-for-IT";
+      name = "Proxmox 1";
+      enable = true;
+      tokenFile = "/var/lib/github-runner/token";
+    };
+  };
+}
